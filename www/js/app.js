6d26f37d0ee7e8bd38040ef64a9562a44af941a0
aaronksaunders <aaron@clearlyinnovative.com>
2015-11-29 17:31:58 -0500

    showed how to upload the file using the REST API

--- www/js/app.js
+++ www/js/app.js
@@ -26,131 +26,180 @@ angular.module('starter', ['ionic', 'ParsePhotoService'])
      * parse constants
      */
     .value('ParseConfiguration', {
-        applicationId: "xxxxxxxxxxxxxxxx",
-        javascriptKey: "xxxxxxxxxxxxxxxx"
+        applicationId: "",
+        javascriptKey: "",
+        restAPIKey: ""
     })
     .config(function ($compileProvider) {
         //    $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|content|file|assets-library):/);
     })
 
-    .controller('MainCtrl', function ($scope, Camera, ParsePhotoService) {
+    .controller('MainCtrl', function ($scope, Camera, ParsePhotoService, ParseRESTPhotoService) {
 
-            /**
-             * make a thumbnail of the image
-             */
-            $scope.makeThumbNail = function () {
+        /** ---------------------------------------------------------------------
+         *
+         * make a thumbnail of the image
+         *
+         --------------------------------------------------------------------- */
+        $scope.makeThumbNail = function () {
 
-                if (!$scope.lastPhoto) {
-                    alert("Missing Photo");
-                    return;
-                }
+            if (!$scope.lastPhoto) {
+                alert("Missing Photo");
+                return;
+            }
 
-                Camera.resizeImage($scope.lastPhoto).then(function (_result) {
-                    $scope.thumb = "data:image/jpeg;base64," + _result.imageData;
+            Camera.resizeImage($scope.lastPhoto).then(function (_result) {
+                $scope.thumb = "data:image/jpeg;base64," + _result.imageData;
+            }, function (_error) {
+                console.log(_error);
+            });
+        };
+
+
+        /** ---------------------------------------------------------------------
+         *
+         * save image to parse USING REST API
+         *
+         --------------------------------------------------------------------- */
+        $scope.savePhotoToParse = function () {
+
+            if (!$scope.lastPhoto) {
+                alert("Missing Photo");
+                return;
+            }
+
+
+            Camera.toBase64Image($scope.lastPhoto).then(function (_result) {
+                var base64 = _result.imageData;
+
+                // make sure we are logged in
+                ParseRESTPhotoService.loginDefaultUser("admin", "password").then(function (_user) {
+                    // user is logged in, now save to parse
+                    return ParseRESTPhotoService.savePhotoToParse({
+                        photo: base64,
+                        caption: "By User " + _user.username
+                    });
+
+                }).then(function (_savePhotoResult) {
+                    console.log("savePhotoToParse ", _savePhotoResult);
+                    alert("Photo Saved Successfully!")
+
                 }, function (_error) {
                     console.log(_error);
+                    alert("savePhotoToParse " + JSON.stringify(_error, null, 2));
                 });
-            };
-
-            /**
-             * save image to parse
-             */
-            $scope.savePhotoToParse = function () {
-
-                if (!$scope.lastPhoto) {
-                    alert("Missing Photo");
-                    return;
-                }
+            });
+        };
 
 
-                Camera.toBase64Image($scope.lastPhoto).then(function (_result) {
-                    var base64 = _result.imageData;
+        /** ---------------------------------------------------------------------
+         *
+         * save image to parse USING JAVASCRIPT API
+         *
+         --------------------------------------------------------------------- */
+        $scope.savePhotoToParse = function () {
 
-                    // make sure we are logged in
-                    ParsePhotoService.loginDefaultUser("admin", "password").then(function (_user) {
-                        // user is logged in, now save to parse
-                        return ParsePhotoService.savePhotoToParse({
-                            photo: base64,
-                            caption: "By User " + _user.get("username")
-                        });
+            if (!$scope.lastPhoto) {
+                alert("Missing Photo");
+                return;
+            }
 
-                    }).then(function (_savePhotoResult) {
-                        console.log("savePhotoToParse ", _savePhotoResult);
 
-                    }, function (_error) {
-                        console.log(_error);
-                        alert("savePhotoToParse " + JSON.stringify(_error, null, 2));
+            Camera.toBase64Image($scope.lastPhoto).then(function (_result) {
+                var base64 = _result.imageData;
+
+                // make sure we are logged in
+                ParsePhotoService.loginDefaultUser("admin", "password").then(function (_user) {
+                    // user is logged in, now save to parse
+                    return ParsePhotoService.savePhotoToParse({
+                        photo: base64,
+                        caption: "By User " + _user.get("username")
                     });
-                });
-            };
 
-            /**
-             * display alert to choose where to get the image from
-             */
-            $scope.getPhoto = function () {
-                var options = {
-                    'buttonLabels': ['Take Picture', 'Select From Gallery'],
-                    'addCancelButtonWithLabel': 'Cancel'
+                }).then(function (_savePhotoResult) {
+                    console.log("savePhotoToParse ", _savePhotoResult);
+
+                }, function (_error) {
+                    console.log(_error);
+                    alert("savePhotoToParse " + JSON.stringify(_error, null, 2));
+                });
+            });
+        };
+
+        /** ---------------------------------------------------------------------
+         *
+         * display alert to choose where to get the image from
+         *
+         --------------------------------------------------------------------- */
+        $scope.getPhoto = function () {
+            var options = {
+                'buttonLabels': ['Take Picture', 'Select From Gallery'],
+                'addCancelButtonWithLabel': 'Cancel'
+            };
+            window.plugins.actionsheet.show(options, callback);
+        };
+
+        function callback(buttonIndex) {
+            console.log(buttonIndex);
+            if (buttonIndex === 1) {
+
+                var picOptions = {
+                    destinationType: navigator.camera.DestinationType.FILE_URI,
+                    quality: 75,
+                    targetWidth: 500,
+                    targetHeight: 500,
+                    allowEdit: true,
+                    saveToPhotoAlbum: false
                 };
-                window.plugins.actionsheet.show(options, callback);
-            };
-
-            function callback(buttonIndex) {
-                console.log(buttonIndex);
-                if (buttonIndex === 1) {
-
-                    var picOptions = {
-                        destinationType: navigator.camera.DestinationType.FILE_URI,
-                        quality: 75,
-                        targetWidth: 500,
-                        targetHeight: 500,
-                        allowEdit: true,
-                        saveToPhotoAlbum: false
-                    };
 
 
-                    Camera.getPicture(picOptions).then(function (imageURI) {
-                        console.log(imageURI);
-                        $scope.lastPhoto = imageURI;
-                        $scope.newPhoto = true;
+                Camera.getPicture(picOptions).then(function (imageURI) {
+                    console.log(imageURI);
+                    $scope.lastPhoto = imageURI;
+                    $scope.newPhoto = true;
 
-                    }, function (err) {
-                        console.log(err);
-                        $scope.newPhoto = false;
-                        alert(err);
-                    });
-                } else if (buttonIndex === 2) {
+                }, function (err) {
+                    console.log(err);
+                    $scope.newPhoto = false;
+                    alert(err);
+                });
+            } else if (buttonIndex === 2) {
 
-                    var picOptions = {
-                        destinationType: navigator.camera.DestinationType.FILE_URI,
-                        quality: 75,
-                        targetWidth: 500,
-                        targetHeight: 500,
-                        sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
-                    };
+                var picOptions = {
+                    destinationType: navigator.camera.DestinationType.FILE_URI,
+                    quality: 75,
+                    targetWidth: 500,
+                    targetHeight: 500,
+                    sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
+                };
 
 
-                    Camera.getPictureFromGallery(picOptions).then(function (imageURI) {
-                        console.log(imageURI);
-                        $scope.lastPhoto = imageURI;
-                        $scope.newPhoto = true;
+                Camera.getPictureFromGallery(picOptions).then(function (imageURI) {
+                    console.log(imageURI);
+                    $scope.lastPhoto = imageURI;
+                    $scope.newPhoto = true;
 
-                    }, function (err) {
-                        console.log(err);
-                        $scope.newPhoto = false;
-                        alert(err);
-                    });
-                }
+                }, function (err) {
+                    console.log(err);
+                    $scope.newPhoto = false;
+                    alert(err);
+                });
+            }
 
-            };
+        };
 
-        }
-    )
+    })
 
     .factory('Camera', ['$q', function ($q) {
 
         return {
 
+            /** ---------------------------------------------------------------------
+             *
+             * @param options
+             * @returns {*}
+             *
+             --------------------------------------------------------------------- */
             getPictureFromGallery: function (options) {
                 var q = $q.defer();
 
@@ -164,11 +213,12 @@ angular.module('starter', ['ionic', 'ParsePhotoService'])
                 return q.promise;
             },
 
-            /**
+            /** ---------------------------------------------------------------------
              *
              * @param options
              * @returns {*}
-             */
+             *
+             --------------------------------------------------------------------- */
             getPicture: function (options) {
                 var q = $q.defer();
 
@@ -181,11 +231,12 @@ angular.module('starter', ['ionic', 'ParsePhotoService'])
 
                 return q.promise;
             },
-            /**
+            /** ---------------------------------------------------------------------
              *
              * @param img_path
              * @returns {*}
-             */
+             *
+             --------------------------------------------------------------------- */
             resizeImage: function (img_path) {
                 var q = $q.defer();
                 window.imageResizer.resizeImage(function (success_resp) {
@@ -206,6 +257,12 @@ angular.module('starter', ['ionic', 'ParsePhotoService'])
                 return q.promise;
             },
 
+            /** ---------------------------------------------------------------------
+             *
+             * @param img_path
+             * @returns {*}
+             *
+             --------------------------------------------------------------------- */
             toBase64Image: function (img_path) {
                 var q = $q.defer();
                 window.imageResizer.resizeImage(function (success_resp) {
--- www/js/parseService.js
+++ www/js/parseService.js
@@ -2,7 +2,82 @@
  * Created by aaronksaunders on 11/27/15.
  */
 angular.module('ParsePhotoService', [])
+    //
+    // Photo Upload Service implemented using the REST API instead of the javascript
+    // library. This was added for completeness since some of the other documentation
+    // I have provided is based on the REST API
+    //
+    .service('ParseRESTPhotoService', ['$http', 'ParseConfiguration',
 
+
+        function ($http, ParseConfiguration) {
+
+            var PARSE__HEADER_CREDENTIALS = {
+                "x-parse-application-id": ParseConfiguration.applicationId,
+                "x-parse-rest-api-key": ParseConfiguration.restAPIKey
+            };
+
+            var baseURL = "https://api.parse.com/1/";
+            var authenticationHeaders = PARSE__HEADER_CREDENTIALS;
+
+            return {
+                loginDefaultUser: function (_user, _password) {
+                    var settings = {
+                        headers: authenticationHeaders,
+                        // params are for query string parameters
+                        params: {
+                            "username": _user,
+                            "password": _password
+                        }
+                    };
+
+                    // $http returns a promise, which has a then function,
+                    // which also returns a promise
+                    return $http.get(baseURL + 'login', settings)
+                        .then(function (response) {
+                            // In the response resp.data contains the result
+                            // check the console to see all of the data returned
+                            console.log('login', response);
+                            return response.data;
+                        });
+                },
+                savePhotoToParse: function (_params) {
+
+                    // for POST, we only need to set the authentication header
+                    var settings = {
+                        headers: authenticationHeaders
+                    };
+                    // for POST, we need to specify data to add, AND convert it to
+                    // a string before passing it in as separate parameter data
+                    var dataObject = {base64: _params.photo};
+
+                    // $http returns a promise, which has a then function
+                    return $http.post(baseURL + 'files/mypic.jpeg', dataObject, settings)
+                        .then(function (response) {
+                            // In the response resp.data contains the result
+                            // check the console to see all of the data returned
+                            console.log('savePhotoToParse', response);
+
+                            // now save to ImageObject Class
+                            return $http.post(baseURL + 'classes/ImageInfo', {
+                                caption: _params.caption,
+                                picture: {
+                                    "name": response.data.name,
+                                    "__type": "File"
+                                }
+                            }, settings);
+                        }).then(function (_imageInfoResp) {
+                            console.log(_imageInfoResp);
+                            return _imageInfoResp.data;
+                        }, function (_error) {
+                            console.log("Error: ", _error);
+                        });
+                }
+            }
+        }])
+    //
+    // Photo Upload Service implemented using the Parse Javascript library.
+    //
     .service('ParsePhotoService', ['$q', 'ParseConfiguration',
         function ($q, ParseConfiguration) {
 
